a96151240a20f6352929b7cd7278683c191666f3
Polina Filipova <polina.filipova@gmail.com>
2015-09-30 14:24:37 -0500

    Close-to-final commit with basic dict-like functionality

--- dict_test.py
+++ dict_test.py
@@ -1,18 +1,45 @@
 "Unit tests for the dictionary module's Dict class."
 
 from dictionary import Dict
-import unittest		# TL;DR
+import unittest   # TL;DR
+
 
 class TestDictionaryFunctions(unittest.TestCase):
 
-	def test_basic(self):
-		d = Dict()
-		d['davin'] = 'green'
-		d['gabriel'] = 'purple'
-		d['mara'] = 'pink'
-		self.assertEqual(d['davin'], 'green')
-		self.assertEqual(d['gabriel'], 'purple')
-		self.assertEqual(d['mara'], 'pink')
-		
+    def test_basic(self):
+        d = Dict()
+        d['davin'] = 'green'
+        d['gabriel'] = 'purple'
+        d['mara'] = 'pink'
+        self.assertEqual(d['davin'], 'green')
+        self.assertEqual(d['gabriel'], 'purple')
+        self.assertEqual(d['mara'], 'pink')
+
+    def test_delete(self):
+        d = Dict()
+        d['davin'] = 'green'
+        d['gabriel'] = 'purple'
+        d['kanai'] = 'silver'
+        del d['davin']
+        with self.assertRaises(KeyError):
+            d['davin']
+
+    def test_update_value_for_an_existing_key(self):
+        d = Dict()
+        d['davin'] = 'green'
+        d['gabriel'] = 'purple'
+        d['kanai'] = 'silver'
+        d['davin'] = 'blue'
+        self.assertEqual(d['davin'], 'blue')
+
+    def test_use_in_a_for_loop(self):
+        d = Dict()
+        d['davin'] = 'green'
+        d['gabriel'] = 'purple'
+        d['mara'] = 'pink'
+        for key in d:
+            pass
+
+			
 if __name__ == '__main__':
-	unittest.main(exit=False)
+    unittest.main(exit=False)
--- dictionary.py
+++ dictionary.py
@@ -1,28 +1,46 @@
-# Re-implementation of Python's dict... only better. Honest.
+"Re-implementation of Python's dict.... only better."
 
 class Dict:
 
-	def __init__(self):
-	# We need the below guys to point to two different places in memory.
-		self.n = 8
-		self.rows = [ [] for i in range(self.n) ]
-		self.desks = [ [] for i in range(self.n) ]
-		
-	def __setitem__(self, key, value):
-		"Implements d[key] = value"
-		# Dunder methods don't really need a doc string but let's leave it.
-		row_id = hash(key) % self.n
-		row = self.rows[row_id]
-		desk = self.desks[row_id]
-		row.append(key)
-		desk.append(value)
-	
-	def __getitem__(self, key):
-		"Implements d[key]"
-		row_id = hash(key) % self.n
-		row = self.rows[row_id]
-		desk = self.desks[row_id]
-		position = row.index(key)
-		value = desk[position]
-		return value
-		
+    def __init__(self):
+        self.n = 8
+        self.rows = [ [] for i in range(self.n) ]
+        self.desks = [ [] for i in range(self.n) ]
+
+    def __setitem__(self, key, value):
+        "Implements d[key] = value"
+        row_id = hash(key) % self.n
+        row = self.rows[row_id]
+        desk = self.desks[row_id]
+        if key not in row:
+            row.append(key)
+            desk.append(value)
+        else:
+            position = row.index(key)
+            desk[position] = value
+
+    def __getitem__(self, key):
+        "Implements d[key]"
+        row_id = hash(key) % self.n
+        row = self.rows[row_id]
+        desk = self.desks[row_id]
+        try:
+            position = row.index(key)
+        except ValueError:
+            raise KeyError(key)
+        value = desk[position]
+        return value
+
+    def __delitem__(self, key):
+        "Implements del d[key]"
+        row_id = hash(key) % self.n
+        row = self.rows[row_id]
+        desk = self.desks[row_id]
+        position = row.index(key)
+        del row[position]
+        del desk[position]
+
+    def __iter__(self):
+        for row in self.rows:
+            for key in row:
+                yield key
